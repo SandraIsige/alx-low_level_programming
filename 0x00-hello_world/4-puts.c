2bca5e4e34cd56d3fcfb8f06006fec7d4a1cfb9c
Sandra <sandraisige@gmail.com>
2023-10-05 14:10:33 -0700

    Task 4 correction 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 /**
- * main - Entry point
- *
- * Description: Outputs the string "Programming is like building
- * a multilingual puzzle" to the main console using the puts function.
- *
- * Return: 0 if successful, otherwise another value
- */
+* main - Entry point
+*
+* Description: Outputs the string "Programming is like building a multilingual puzzle to the main console using the puts function.
+*
+* Return: 0 if successful, otherwise another value
+*/
 #include <stdio.h>
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
