8c35a0a6be8813270f394173c6e268f2e88ad824
Sandra <sandraisige@gmail.com>
2023-10-05 14:21:59 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Prints "with proper grammar, but the outcome is
+* a piece of art," followed by a new line.
+*
+* Return: 0 if successful, otherwise a non-zero value
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
