088439508ae32f32467f6ae617f11c53122ed84d
Sandra <sandraisige@gmail.com>
2023-10-05 13:11:15 -0700

    Task 4 correction 1.1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#include <stdio.h>
-int main(void) { puts("\"Programming is like building a multilingual puzzle"); return (0); }
+/**
+  * @brief
+  *
+  * This function outputs the string "Programming is like building a multilingual puzzle to the console using the puts function
+  *
+  * @return Returns 0 upon successful execution
+  */
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
