47ae1e20e7eb1e73511e72e54bf1c53ad3d1a6ac
Sandra <sandraisige@gmail.com>
2023-10-11 04:58:51 -0700

    Task 2 correction 1.0

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 
 	for (count = 0; count < 10; count++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter ++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
