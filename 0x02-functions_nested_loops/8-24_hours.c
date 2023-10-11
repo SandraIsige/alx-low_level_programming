f514f23883cf11911ca7bab35fef84b570d1d8ce
Sandra <sandraisige@gmail.com>
2023-10-11 07:38:11 -0700

    Task 8 correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* jack _bauer - Prints every minute of Jack Bauer's day from 00:00 to 23:59.
+* jack _bauer - Prints every minute of the day from 00:00 to 23:59.
 */
 void jack_bauer(void)
 {
