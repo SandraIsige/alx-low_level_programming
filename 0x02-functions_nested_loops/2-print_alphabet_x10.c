792e1f28ccf6fe21205d5fb55f84d2067fa98296
Sandra <sandraisige@gmail.com>
2023-10-11 04:44:45 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints the lowercase alphabet 10 times
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		print_alphabet();
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 #include <unistd.h> /* for write() */
 
 int _putchar(char c);
-void print_alphabet(void);
+void print_alphabet(void)
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
