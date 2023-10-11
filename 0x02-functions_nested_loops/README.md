1cf9d7184f0c82261acc0c13867a9045f52a14db
Sandra <sandraisige@gmail.com>
2023-10-11 03:06:32 -0700

    Task 0 partial

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This readme is for the '0x02. C - Functions, nested loops' project.
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+/* main.h */
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_string(void);
+
+endif /* MAIN_H */
