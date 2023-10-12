94dfdb5b476b2645b02950723549eef37baf36b7
Sandra <sandraisige@gmail.com>
2023-10-12 15:29:11 -0700

    Task 9 correction 1.1

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,21 @@
 */
 void fizz_buzz(void)
 {
-    int x;
+	int x;
 
-    for (x = 1; x <= 100; x++)
-    {
-        if (x % 3 == 0 && x % 5 == 0)
-            printf("FizzBuzz");
-        else if (x % 3 == 0)
-            printf("Fizz");
-        else if (x % 5 == 0)
-            printf("Buzz");
-        else
-            printf("%d", x);
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz");
+		else if (x % 3 == 0)
+			printf("Fizz");
+		else if (x % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", x);
 
-        if (x != 100)
-            printf(" ");
-    }
-    printf("\n");
+		if (x != 100)
+			printf(" ");
+	}
+	printf("\n");
 }
