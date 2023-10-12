7b9a3e2ab61002548f37dc867215674434b0d26e
Sandra <sandraisige@gmail.com>
2023-10-12 11:13:09 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase characters.
+* @C: The character to be checked.
+*
+* Return: 1 if uppercase, 0 otherwise.
+*/
+int _isupper(int c)
+{
+	if (C >= 'A' && C <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
